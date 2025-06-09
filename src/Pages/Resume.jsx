6bec9912143df9f512c462a20f2214f3ad1b2973
Deniya-Edwinraj <deniyaedwinraj@gmail.com>
2025-06-09 09:38:04 +0530

    motion frame polished

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import axios from "axios";
-import { educationtimelineData, experienceData, Sskills, Tskills } from "../Constants/Data";
+import {
+  educationtimelineData,
+  experienceData,
+  Sskills,
+  Tskills,
+} from "../Constants/Data";
 
 const API_KEY = "AIzaSyBv2Ktmv1ozdJKCcCIB1nOEhlAKVajfE6w";
 const ROOT_FOLDER_ID = "1r2BeGq7IZD_CE9bLwQ1D7dsSI9xLuNLu";
@@ -186,156 +191,181 @@ export default function Resume() {
           {/* Show skills only on that tab */}
           {activeTab === "Professional Skills" && (
             <>
-              <>
-                <div className="row g-4">
-                  <p className="fs-5 text-secondary">
-                    Programming Languages & Libraries
-                  </p>
-                  {Tskills.programming.map((skill, index) => (
-                    <div key={index} className="col-md-2">
-                      <div className="p-2 shadow-box rounded-4 h-100 text-center">
-                        <div className="fs-3 text-danger">{skill.icon}</div>
-                        <p className="mt-2 small">{skill.name}</p>
+              <motion.div
+                initial={{ opacity: 0, x: 100, y: -50 }}
+                whileInView={{ opacity: 1, x: 0, y: 0 }}
+                transition={{ duration: 0.6, ease: "easeOut" }}
+                className="col-md-12"
+              >
+                <>
+                  <div className="row g-4">
+                    <p className="fs-5 text-secondary">
+                      Programming Languages & Libraries
+                    </p>
+                    {Tskills.programming.map((skill, index) => (
+                      <div key={index} className="col-md-2">
+                        <div className="p-2 shadow-box rounded-4 h-100 text-center">
+                          <div className="fs-3 text-danger">{skill.icon}</div>
+                          <p className="mt-2 small">{skill.name}</p>
+                        </div>
                       </div>
-                    </div>
-                  ))}
-                </div>
-
-                <div className="row g-4 mt-4">
-                  <p className="fs-5 text-secondary">Styling</p>
-                  {Tskills.styling.map((skill, index) => (
-                    <div key={index} className="col-md-2">
-                      <div className="p-2 shadow-box rounded-4 h-100 text-center">
-                        <div className="fs-3 text-danger">{skill.icon}</div>
-                        <p className="mt-2 small">{skill.name}</p>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-
-                <div className="row g-4 mt-4">
-                  <p className="fs-5 text-secondary">
-                    Tools & Productivity Software
-                  </p>
-                  {Tskills.productivityTools.map((skill, index) => (
-                    <div key={index} className="col-md-2">
-                      <div className="p-2 shadow-box rounded-4 h-100 text-center">
-                        <div className="fs-3 text-danger">{skill.icon}</div>
-                        <p className="mt-2 small">{skill.name}</p>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-
-                <div className="row g-4 mt-4">
-                  <p className="fs-5 text-secondary">
-                    Project Management Skills
-                  </p>
-                  {Tskills.projectManagement.map((skill, index) => (
-                    <div key={index} className="col-md-2">
-                      <div className="p-2 shadow-box rounded-4 h-100 text-center">
-                        <div className="fs-3 text-danger">{skill.icon}</div>
-                        <p className="mt-2 small">{skill.name}</p>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </>
-
-              <div className="row g-4 mt-4">
-                <p className="fs-5 text-secondary">Interpersonal Skills</p>
-                {Sskills.map((skill, index) => (
-                  <div key={index} className="col-md-2">
-                    <div className="p-2 shadow-box rounded-4 h-100 text-center">
-                      <div className="card-img-container overflow-hidden rounded-3 fs-3">
-                        <span className="text-danger">{skill.icon}</span>
-                      </div>
-                      <p className="mt-2 small">{skill.name}</p>
-                    </div>
+                    ))}
                   </div>
-                ))}
-              </div>
+
+                  <div className="row g-4 mt-4">
+                    <p className="fs-5 text-secondary">Styling</p>
+                    {Tskills.styling.map((skill, index) => (
+                      <div key={index} className="col-md-2">
+                        <div className="p-2 shadow-box rounded-4 h-100 text-center">
+                          <div className="fs-3 text-danger">{skill.icon}</div>
+                          <p className="mt-2 small">{skill.name}</p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+
+                  <div className="row g-4 mt-4">
+                    <p className="fs-5 text-secondary">
+                      Tools & Productivity Software
+                    </p>
+                    {Tskills.productivityTools.map((skill, index) => (
+                      <div key={index} className="col-md-2">
+                        <div className="p-2 shadow-box rounded-4 h-100 text-center">
+                          <div className="fs-3 text-danger">{skill.icon}</div>
+                          <p className="mt-2 small">{skill.name}</p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+
+                  <div className="row g-4 mt-4">
+                    <p className="fs-5 text-secondary">
+                      Project Management Skills
+                    </p>
+                    {Tskills.projectManagement.map((skill, index) => (
+                      <div key={index} className="col-md-2">
+                        <div className="p-2 shadow-box rounded-4 h-100 text-center">
+                          <div className="fs-3 text-danger">{skill.icon}</div>
+                          <p className="mt-2 small">{skill.name}</p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </>
+
+                <div className="row g-4 mt-4">
+                  <p className="fs-5 text-secondary">Interpersonal Skills</p>
+                  {Sskills.map((skill, index) => (
+                    <div key={index} className="col-md-2">
+                      <div className="p-2 shadow-box rounded-4 h-100 text-center">
+                        <div className="card-img-container overflow-hidden rounded-3 fs-3">
+                          <span className="text-danger">{skill.icon}</span>
+                        </div>
+                        <p className="mt-2 small">{skill.name}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </motion.div>
             </>
           )}
 
-                    {/* Show experience only on that tab */}
+          {/* Show experience only on that tab */}
           {activeTab === "Experience" && (
-            <div className="row position-relative">
-              <h6 className="text-danger fw-bold">2024 - Present</h6>
-              <h2 className="fw-bold mb-4">Experience</h2>
-              <div className="row">
-                <div className="col-md-6">
-                  <ul className="timeline position-relative my-0 mx-auto py-2 list-unstyled">
-                    {experienceData
-                      .slice(0, Math.ceil(experienceData.length / 2))
-                      .map((item, index) => (
-                        <li key={index} className="py-3">
-                          <div className="direction-r position-relative">
-                            <div className="card bg-dark mb-4 text-white">
-                              <span className="flag position-relative d-inline rounded"></span>
-                              <div className="card-body position-relative rounded-3 p-3 shadow-box mx-3">
-                                <h5 className="card-title">{item.title}</h5>
-                                <p className="text-secondary small">
-                                  {item.role} ( {item.year} )
-                                </p>
-                                <p className="card-text">{item.description}</p>
+            <motion.div
+              initial={{ opacity: 0, x: 100, y: -50 }}
+              whileInView={{ opacity: 1, x: 0, y: 0 }}
+              transition={{ duration: 0.6, ease: "easeOut" }}
+              className="col-md-12"
+            >
+              <div className="row position-relative">
+                <h6 className="text-danger fw-bold">2024 - Present</h6>
+                <h2 className="fw-bold mb-4">Experience</h2>
+                <div className="row">
+                  <div className="col-md-6">
+                    <ul className="timeline position-relative my-0 mx-auto py-2 list-unstyled">
+                      {experienceData
+                        .slice(0, Math.ceil(experienceData.length / 2))
+                        .map((item, index) => (
+                          <li key={index} className="py-3">
+                            <div className="direction-r position-relative">
+                              <div className="card bg-dark mb-4 text-white">
+                                <span className="flag position-relative d-inline rounded"></span>
+                                <div className="card-body position-relative rounded-3 p-3 shadow-box mx-3">
+                                  <h5 className="card-title">{item.title}</h5>
+                                  <p className="text-secondary small">
+                                    {item.role} ( {item.year} )
+                                  </p>
+                                  <p className="card-text">
+                                    {item.description}
+                                  </p>
+                                </div>
                               </div>
                             </div>
-                          </div>
-                        </li>
-                      ))}
-                  </ul>
-                </div>
+                          </li>
+                        ))}
+                    </ul>
+                  </div>
 
-                <div className="col-md-6">
-                  <ul className="timeline position-relative my-0 mx-auto py-2 list-unstyled">
-                    {experienceData
-                      .slice(Math.ceil(experienceData.length / 2))
-                      .map((item, index) => (
-                        <li key={index} className="py-3">
-                          <div className="direction-r position-relative">
-                            <div className="card bg-dark mb-4 text-white">
-                              <span className="flag position-relative d-inline rounded"></span>
-                              <div className="card-body position-relative rounded-3 p-3 shadow-box mx-3">
-                                <h5 className="card-title">{item.title}</h5>
-                                <p className="text-secondary small">
-                                  {item.role}  ( {item.year} )
-                                </p>
-                                <p className="card-text">{item.description}</p>
+                  <div className="col-md-6">
+                    <ul className="timeline position-relative my-0 mx-auto py-2 list-unstyled">
+                      {experienceData
+                        .slice(Math.ceil(experienceData.length / 2))
+                        .map((item, index) => (
+                          <li key={index} className="py-3">
+                            <div className="direction-r position-relative">
+                              <div className="card bg-dark mb-4 text-white">
+                                <span className="flag position-relative d-inline rounded"></span>
+                                <div className="card-body position-relative rounded-3 p-3 shadow-box mx-3">
+                                  <h5 className="card-title">{item.title}</h5>
+                                  <p className="text-secondary small">
+                                    {item.role} ( {item.year} )
+                                  </p>
+                                  <p className="card-text">
+                                    {item.description}
+                                  </p>
+                                </div>
                               </div>
                             </div>
-                          </div>
-                        </li>
-                      ))}
-                  </ul>
+                          </li>
+                        ))}
+                    </ul>
+                  </div>
                 </div>
               </div>
-            </div>
+            </motion.div>
           )}
 
           {/* Show certifications only on that tab */}
           {activeTab === "Certifications" && (
-            <div className="row g-4">
-              {certificates.map((file, index) => (
-                <div key={index} className="col-md-3">
-                  <div className="p-4 shadow-box rounded-4 h-100">
-                    <div className="card-img-container overflow-hidden rounded-3">
-                      <img
-                        src={getDrivePreview(file)}
-                        onError={(e) => {
-                          e.target.src =
-                            "https://placehold.co/250x180?text=Preview+Unavailable";
-                        }}
-                        className="card-img-top w-100 zoom-on-hover"
-                        alt={file.name}
-                        style={{ height: "250px", objectFit: "cover" }}
-                      />
+            <motion.div
+              initial={{ opacity: 0, x: 100, y: -50 }}
+              whileInView={{ opacity: 1, x: 0, y: 0 }}
+              transition={{ duration: 0.6, ease: "easeOut" }}
+              className="col-md-12"
+            >
+              <div className="row g-4">
+                {certificates.map((file, index) => (
+                  <div key={index} className="col-md-3">
+                    <div className="p-4 shadow-box rounded-4 h-100">
+                      <div className="card-img-container overflow-hidden rounded-3">
+                        <img
+                          src={getDrivePreview(file)}
+                          onError={(e) => {
+                            e.target.src =
+                              "https://placehold.co/250x180?text=Preview+Unavailable";
+                          }}
+                          className="card-img-top w-100 zoom-on-hover"
+                          alt={file.name}
+                          style={{ height: "250px", objectFit: "cover" }}
+                        />
+                      </div>
+                      <p className="mt-2 text-center small">{file.name}</p>
                     </div>
-                    <p className="mt-2 text-center small">{file.name}</p>
                   </div>
-                </div>
-              ))}
-            </div>
+                ))}
+              </div>
+            </motion.div>
           )}
         </motion.div>
       </div>
